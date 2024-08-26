5764de0600d98c1b8fc6fb9529c7521dc5f40a46
shihab <shahriarshihab96@gmail.com>
2024-08-27 00:05:05 +0600

    added function

--- index.js
+++ index.js
@@ -1,7 +1,28 @@
-function Person(age,name) {
-    const person = Object.create(); 
+const personMethods = {
+    eat() {
+        console.log(`Person is eating`);
+    },
+    sleep() {
+        console.log(`Person is sleeping`);
+    },
+    play() {
+        console.log(`Person is playing`);
+    },
+}; 
+
+function Person(age, name) {
+    const person = Object.create(personMethods); 
     person.age = age; 
     person.name = name;
+
+
+    // not necessary
+    // person.eat = personMethods.eat; 
+    // person.sleep = personMethods.sleep; 
+    // person.play = personMethods.play;
+
     return person; 
 }
 
+const rakib = Person("Rakib", 44); 
+rakib.play(); 
